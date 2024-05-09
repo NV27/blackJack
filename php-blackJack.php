0a83aee9ecebdfe4479fc0372cc87caa1c9d8596
Louis <louisprotano@hotmail.com>
2024-05-09 11:02:36 +0100

    Working with suits now too

--- php-blackJack.php
+++ php-blackJack.php
@@ -36,7 +36,34 @@ $player2 = array(
     'score' => 0
 );
 
-$cardArray = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
+function makeDeck() : array{
+
+    $cardArray = [];
+    for ($i = 0; $i < 4; $i++){
+        if ($i === 0) {$suit = "\u{2660}";}
+        if ($i === 1) {$suit = "\u{2665}";}
+        if ($i === 2) {$suit = "\u{2666}";}
+        if ($i === 3) {$suit = "\u{2663}";}
+        array_push($cardArray,
+            ['suit' => $suit, 'face' => 'A', 'value' => 11],
+            ['suit' => $suit, 'face' => 2, 'value' => 2],
+            ['suit' => $suit, 'face' => 3, 'value' => 3],
+            ['suit' => $suit, 'face' => 4, 'value' => 4],
+            ['suit' => $suit, 'face' => 5, 'value' => 5],
+            ['suit' => $suit, 'face' => 6, 'value' => 6],
+            ['suit' => $suit, 'face' => 7, 'value' => 7],
+            ['suit' => $suit, 'face' => 8, 'value' => 8],
+            ['suit' => $suit, 'face' => 9, 'value' => 9],
+            ['suit' => $suit, 'face' => 10, 'value' => 10],
+            ['suit' => $suit, 'face' => 'J', 'value' => 10],
+            ['suit' => $suit, 'face' => 'Q', 'value' => 10],
+            ['suit' => $suit, 'face' => 'K', 'value' => 10]
+        );
+    }
+    return $cardArray;
+}
+
+$cardArray = makeDeck();
 
 function makeValues($card) : int{
     if (is_int($card)){
@@ -52,45 +79,18 @@ function makeValues($card) : int{
 
 if (isset($_GET['submit'])){
     shuffle($cardArray);
+//    echo '<pre>';
 //    var_dump($cardArray);
     echo '<br> <br>';
     $player1['card1'] = array_shift($cardArray);
     $player1['card2'] = array_shift($cardArray);
-//    $player1['card3'] = array_shift($cardArray);
-//    $player1['card4'] = array_shift($cardArray);
-//    $player1['card5'] = array_shift($cardArray);
+
     $player2['card1'] = array_shift($cardArray);
     $player2['card2'] = array_shift($cardArray);
-//    $player2['card3'] = array_shift($cardArray);
-//    $player2['card4'] = array_shift($cardArray);
-//    $player2['card5'] = array_shift($cardArray);
 
-    $a1 = makeValues($player1['card1']);
-    $a2 = makeValues($player1['card2']);
-    $a3 = makeValues($player1['card3']);
-    $a4 = makeValues($player1['card4']);
-    $a5 = makeValues($player1['card5']);
+    $player1['score'] = $player1['card1']['value'] + $player1['card2']['value'];
+    $player2['score'] = $player2['card1']['value'] + $player2['card2']['value'];
 
-    $b1 = makeValues($player2['card1']);
-    $b2 = makeValues($player2['card2']);
-    $b3 = makeValues($player2['card3']);
-    $b4 = makeValues($player2['card4']);
-    $b5 = makeValues($player2['card5']);
-
-//    $draw1 = ($player1['score'] < 14);
-//    $draw2 = ($player2['score'] < 14);
-
-
-//    for($i = 0; $player1['score'] <= 14; $i++){
-//        $player1['score'] = $a1+$a2+($a3 * ($i===3))+($a4 * ($i===4))+($a5 * ($i===5));
-//    }
-//
-//    for($j = 0; $player2['score'] <= 14; $j++){
-//        $player2['score'] = $b1+$b2+($b3 * ($i===3))+($b4 * ($i===4))+($b5 * ($i===5));
-//    }
-
-    $player1['score'] = $a1+$a2;
-    $player2['score'] = $b1+$b2;
 
     if (($player1['score'] - $player2['score']) > 0){
         $winner = 'Player 1';
@@ -103,26 +103,27 @@ if (isset($_GET['submit'])){
     }
 
     echo "PLAYER 1 <br>";
-    echo "Card 1: " . $player1['card1'];
+    echo 'Card 1: ' . ($player1['card1']['face']) . ' ' . ($player1['card1']['suit']);
     echo '<br>';
-    echo "Card 2: " . $player1['card2'];
-    echo '<br>';
-    if ($player1['card3'] > 0) {echo "Card 3: " . $player1['card3'];}
+    echo 'Card 2: ' . ($player1['card2']['face']) . ' ' . ($player1['card2']['suit']);
+//    echo '<br>';
+//    if ($player1['card3'] > 0) {echo "Card 3: " . $player1['card3'];}
     echo '<br>';
     echo "Score: " . $player1['score'];
 
     echo '<br> <br>';
     echo "PLAYER 2 <br>";
-    echo "Card 1: " . $player2['card1'];
+    echo 'Card 1: ' . ($player2['card1']['face']) . ' ' .($player2['card1']['suit']);
     echo '<br>';
-    echo "Card 2: " . $player2['card2'];
-    echo '<br>';
-    if ($player2['card3'] > 0) {echo "Card 3: " . $player2['card3'];}
+    echo 'Card 2: ' . ($player2['card2']['face']) . ' ' . ($player2['card2']['suit']);
+//    echo '<br>';
+//    if ($player2['card3'] > 0) {echo "Card 3: " . $player2['card3'];}
     echo '<br>';
     echo "Score: " . $player2['score'];
 
     echo '<br> <br>';
     echo "$winner is the winner";
+
 }
 
 ?>
